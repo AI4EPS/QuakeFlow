bd86336654276f1d68edbe03db1370fd064ceb5b
Weiqiang <zhuwq0@gmail.com>
2023-05-14 15:41:26 -0700

    update slurm data processing

--- slurm/run_eqnet.py
+++ slurm/run_eqnet.py
@@ -1,9 +1,16 @@
 # %%
 from pathlib import Path
 import os
+import torch
 
 # %%
-mseed_path = Path("waveforms")
+# region = "Hawaii_Loa"
+region = "South_Pole"
+region = "South_Pole2"
+root_path = Path(region)
+
+# %%
+mseed_path = root_path / "waveforms"
 mseeds = sorted(list(mseed_path.rglob("*.mseed")))
 file_list = []
 for f in mseeds:
@@ -15,8 +22,11 @@ file_list = sorted(list(set(file_list)))
 with open("mseed_list.txt", "w") as fp:
     fp.write("\n".join(file_list))
 
+# %%
+num_gpu = torch.cuda.device_count()
 
 # %%
-os.system("torchrun --standalone --nproc_per_node 4 ../EQNet/predict.py --model phasenet --add_polarity --add_event --data_path ./ --data_list mseed_list.txt  --response_xml 'stations/*xml'  --result_path ./eqnet_picks --batch_size=1 --format mseed")
+# os.system(f"torchrun --standalone --nproc_per_node 4 ../EQNet/predict.py --model phasenet --add_polarity --add_event --data_path ./ --data_list mseed_list.txt  --response_xml '{root_path}/stations/*xml'  --result_path ./eqnet_picks --batch_size=1 --format mseed")
+os.system(f"torchrun --standalone --nproc_per_node {num_gpu} ../EQNet/predict.py --model phasenet --add_polarity --add_event --data_path ./ --data_list mseed_list.txt  --response_xml '{root_path}/response.xml'  --result_path {root_path}/eqnet_picks --batch_size=1 --format mseed")
 
-os.system("cp eqnet_picks/picks_phasenet_raw.csv results/picks.csv")
+os.system(f"cp {root_path}/eqnet_picks/picks_phasenet_raw.csv {root_path}/results/picks_eqnet.csv")
--- slurm/run_phasenet.py
+++ slurm/run_phasenet.py
@@ -3,17 +3,26 @@ from pathlib import Path
 import os
 
 # %%
-waveform_path = Path("waveforms")
+# region = "Hawaii_Loa"
+region = "South_Pole"
+root_path = Path(region)
+
+# %%
+waveform_path = root_path / "waveforms"
 mseed_list = sorted(list(waveform_path.rglob("*.mseed")))
-mseeds = []
+file_list = []
 for f in mseed_list:
-    mseeds.append(str(f).split(".mseed")[0][:-1]+"*.mseed")
+    file_list.append(str(f).split(".mseed")[0][:-1]+"*.mseed")
+
+file_list = sorted(list(set(file_list)))
 
 # %%
-with open("mseed.csv", "w") as fp:
+with open("mseed_list.csv", "w") as fp:
     fp.write("fname\n")
-    fp.write("\n".join(mseeds))
-
+    fp.write("\n".join(file_list))
 
 # %%
-os.system("python ../PhaseNet/phasenet/predict.py --model=../PhaseNet/model/190703-214543 --data_list=mseed.csv --data_dir=./ --format=mseed --amplitude --response_xml=stations/*xml --batch_size=1")
+# os.system(f"python ../PhaseNet/phasenet/predict.py --model=../PhaseNet/model/190703-214543 --data_list=mseed.csv --data_dir=./ --format=mseed --amplitude --response_xml={root_path}/stations/*xml --batch_size=1")
+os.system(f"python ../PhaseNet/phasenet/predict.py --model=../PhaseNet/model/190703-214543 --data_list=mseed_list.csv --data_dir=./ --format=mseed --amplitude --response_xml={root_path}/response.xml --result_dir={root_path}/phasenet_picks/  --batch_size=1")
+
+os.system(f"cp {root_path}/phasenet_picks/picks.csv {root_path}/results/picks_phasenet.csv")
