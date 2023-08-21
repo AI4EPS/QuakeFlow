e72dfbdbf39b96865eeabe5697ea9ef2863f7dc9
zhuwq <zhuwq0@gmail.com>
2023-08-20 22:01:06 -0700

    update main.py

--- quakeflow/main.py
+++ quakeflow/main.py
@@ -7,16 +7,33 @@ I want to use Python, FastAPI, Websockets, Plotly.js, and Redis Stream.
 Can you show me a complete example line by line. Only output code, no explanation.
 """
 
-from fastapi import FastAPI, WebSocket
-import redis
-import json
 import asyncio
-import numpy as np
+import json
 import logging
+from collections import defaultdict, deque
 from datetime import datetime
-from collections import deque
+
+import numpy as np
+import redis
+import requests
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+import websockets
 
 logging.basicConfig(level=logging.INFO)
+
+## For fastapi
+PHASE_PICKER_API = "ws://127.0.0.1:8001"
+# PHASE_ASSOCIATOR_API = None
+# FOCAL_MECHANISM_API = None
+# EARTHQUAKE_LOCATION_API = None
+# EARTHQUAKE_RELOCATION_API = None
+## For gradio
+# PHASE_PICKING_MODEL = None
+# PHASE_ASSOCIATION_MODEL = None
+# FOCAL_MECHANISM_MODEL = None
+# EARTHQUAKE_LOCATION_MODEL = None
+# EARTHQUAKE_RELOCATION_MODEL = None
 
 app = FastAPI()
 r = redis.Redis(host="localhost", port=6379, db=0)
@@ -33,6 +50,74 @@ data = {id: deque(maxlen=MAXQUEUE) for id in station_ids}
 timestamp = {id: deque(maxlen=MAXQUEUE) for id in station_ids}
 
 normalize = lambda x: (x - np.mean(x)) / np.std(x) / 6.0
+
+
+# websocket_endpoint_phase_picker = websockets.connect("ws://127.0.0.1:8001/ws")
+# print("Connected to phase picker")
+
+
+@app.on_event("startup")
+async def startup():
+    global websocket_endpoint_phase_picker
+    websocket_endpoint_phase_picker = await websockets.connect("ws://127.0.0.1:8001/ws")
+    print("Connected to phase picker")
+
+
+async def send(websocket, payload):
+    # await websocket.send(payload)
+    await websocket.send(json.dumps(payload))
+    print("Sent ")
+
+
+async def receive(websocket):
+    response = await websocket.recv()
+    print("Received ")
+    return response
+
+
+@app.get("/")
+async def get():
+    with open("index.html", "r") as f:
+        return HTMLResponse(f.read())
+
+
+def preprocess(meta, sample_rate=100.0, padding=True):
+    if padding:
+        begin_time = min([meta[id]["timestamp"][0] for id in meta])
+        end_time = max([meta[id]["timestamp"][-1] for id in meta])
+    else:
+        begin_time = max([meta[id]["timestamp"][0] for id in meta])
+        end_time = min([meta[id]["timestamp"][-1] for id in meta])
+
+    comp = ["3", "2", "1", "E", "N", "Z"]
+    order = {key: i for i, key in enumerate(comp)}
+    comp2idx = {"3": 0, "2": 1, "1": 2, "E": 0, "N": 1, "Z": 2}  ## for cases less than 3 components
+
+    station_ids = defaultdict(list)
+    for id in meta:
+        station_ids[id[:-1]].append(id[-1])
+
+    nx = len(station_ids)
+    # nt = (end_time - begin_time).seconds * sample_rate + 1
+    nt = int(round((end_time - begin_time) * sample_rate)) + 1
+    data = np.zeros([3, nt, nx], dtype=np.float32)
+    for i, s in enumerate(station_ids):
+        for j, c in enumerate(sorted(station_ids[s], key=lambda x: order[x])):
+            if len(station_ids[s]) != 3:  ## less than 3 component
+                j = comp2idx[c]
+
+            trace = meta[f"{s}{c}"]["data"]
+            it0 = int(round((meta[f"{s}{c}"]["timestamp"][0] - begin_time) * sample_rate))
+            # print(it0, it0 + len(trace), len(trace), nt, begin_time, end_time, meta[f"{s}{c}"]["timestamp"][0])
+            data[j, it0 : it0 + len(trace), i] = trace
+
+    meta = {
+        "id": list(station_ids.keys()),
+        # "vec": data.tolist(),
+        "vec": np.transpose(data, [2, 1, 0]).tolist(),
+        "timestamp": [datetime.fromtimestamp(begin_time).isoformat()] * nx,
+    }
+    return meta
 
 
 @app.websocket("/ws")
@@ -59,9 +144,29 @@ async def websocket_endpoint(websocket: WebSocket):
                     timestamp[sid].extend(timestamp_)
             waveforms[sid] = {
                 "data": (normalize(np.array(data[sid])) + i).tolist(),
-                "timestamp": [datetime.fromtimestamp(x).isoformat() for x in timestamp[sid]],
+                # "timestamp": [datetime.fromtimestamp(x).isoformat() for x in timestamp[sid]],
+                "timestamp": list(timestamp[sid]),
             }
             # await websocket.send_json({id: {"data": list(data[id]), "timestamp": list(timestamp[id])}})
+
+        payload = preprocess(waveforms)
+        # resp = requests.post(f"{PHASE_PICKER_API}/predict", json=payload)
+        # if resp.status_code == 200:
+        #     picks = resp.json()
+        #     print(picks)
+
+        # await send(websocket_endpoint_phase_picker, payload)
+        # picks = await receive(websocket_endpoint_phase_picker)
+        out_s = send(websocket_endpoint_phase_picker, payload)
+        out_r = receive(websocket_endpoint_phase_picker)
+        _, picks = await asyncio.gather(out_s, out_r)
+        print(picks)
+        ## events = requests.post(PHASE_ASSOCIATION_API, json={"picks": picks, "stations": stations})"})
+        ## mechanisms = requests.post(FOCAL_MECHANISM_API, json={"picks": picks, "events": events, "stations": stations})
+        ## relocations = requests.post(EARTHQUAKE_RELOCATION_API, json={"picks": picks, "events": events, "stations": stations})
+
+        # for sid in waveforms:
+        #     waveforms[sid]["timestamp"] = [datetime.fromtimestamp(x).isoformat() for x in waveforms[sid]["timestamp"]]
         await websocket.send_json(waveforms)
         logging.info("Updating...")
         await asyncio.sleep(1)
