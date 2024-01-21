b55d8269341e7717560d8d6483621f35d757250c
zhuwq <zhuwq0@gmail.com>
2024-01-20 22:05:56 -0800

    update

--- slurm/run_phasenet_ncedc.py
+++ slurm/run_phasenet_ncedc.py
@@ -4,26 +4,22 @@ from typing import Dict, List
 from kfp import dsl
 
 
-@dsl.component(base_image="zhuwq0/phasenet-ncedc:v1.1", packages_to_install=["fsspec", "gcsfs", "s3fs"])
-def run_phasenet(
+@dsl.component(packages_to_install=["fsspec", "gcsfs", "s3fs", "tqdm"])
+def get_mseed_list(
     root_path: str,
     region: str,
     config: Dict,
     rank: int = 0,
-    model_path: str = "./",
-    mseed_list: List = None,
+    year: int = 2023,
     protocol: str = "s3",
     bucket: str = "",
     token: Dict = None,
-) -> int:
+) -> str:
     # %%
-    import json
-    import os
     from collections import defaultdict
-    from glob import glob
+    from concurrent.futures import ThreadPoolExecutor
 
     import fsspec
-    import obspy
     from tqdm import tqdm
 
     # %%
@@ -32,26 +28,27 @@ def run_phasenet(
     folder = "continuous_waveforms"
     fs = fsspec.filesystem(protocol=protocol, anon=True)
 
-    # %%
-    valid_channels = ["3", "2", "1", "E", "N", "Z"]
-    valid_instruments = ["BH", "HH", "EH", "HN", "DP"]
+    # # %%
+    # valid_channels = ["3", "2", "1", "E", "N", "Z"]
+    # valid_instruments = ["BH", "HH", "EH", "HN", "DP"]
 
-    # %%
+    # # %%
     # mseed_dir = "mseed_list"
     # if not os.path.exists(f"{mseed_dir}"):
     #     os.makedirs(f"{mseed_dir}")
 
-    # for year in range(2023, 2024):
-    #     networks = fs.glob(f"{bucket}/{folder}/*")
-    #     for i, network in enumerate(tqdm(networks)):
-    #         mseed_list = []
-    #         # years = fs.glob(f"{network}/????")
-    #         # for year in years:
-    #         jdays = fs.glob(f"{network}/{year}/????.???")
-    #         for jday in jdays:
-    #             mseeds = fs.glob(f"{jday}/*.{jday.split('/')[-1]}")
-    #             mseed_list.extend(mseeds)
-
+    # # for year in range(1999, 2024)[::-1]:
+    # def process(year):
+    #     networks = fs.glob(f"{bucket}/{folder}/??")
+    #     pbar = tqdm(networks, desc=f"{year}")
+    #     for i, network in enumerate(pbar):
+    #         pbar.set_description(f"{year} {network.split('/')[-1]}")
+    #         # mseed_list = []
+    #         # jdays = fs.glob(f"{network}/{year}/????.???")
+    #         # for jday in jdays:
+    #         #     mseeds = fs.glob(f"{jday}/*.{jday.split('/')[-1]}")
+    #         #     mseed_list.extend(mseeds)
+    #         mseed_list = fs.glob(f"{network}/{year}/{year}.???/*.{year}.???")
     #         mseed_list = sorted([f"{protocol}://{mseed}" for mseed in mseed_list])
     #         if len(mseed_list) > 0:
     #             with open(f"{mseed_dir}/{year}_{network.split('/')[-1]}.txt", "w") as fp:
@@ -72,25 +69,82 @@ def run_phasenet(
     #                 for key in keys:
     #                     fp.write(",".join(sorted(groups[key])) + "\n")
 
-    # %%
-    world_size = config["world_size"]
+    # num_cores = 32
+    # with ThreadPoolExecutor(max_workers=num_cores) as executor:
+    #     futures = [executor.submit(process, year) for year in range(1999, 2024)]
 
+    # raise
+    # %%
     fs = fsspec.filesystem("gs", token=token)
     bucket = "quakeflow_catalog"
     folder = "NC"
+    networks = fs.glob(f"{bucket}/{folder}/mseed_list/{year}_??_3c.txt")
+    networks = [x.split("/")[-1].split("_")[1] for x in networks]
     networks = ["NC", "BK"]
     mseed_list = []
     for network in networks:
-        with fs.open(f"{bucket}/{folder}/mseed_list/2023_{network}_3c.txt", "r") as fp:
-            mseed_list.extend(fp.read().splitlines())
+        if fs.exists(f"{bucket}/{folder}/mseed_list/{year}_{network}_3c.txt"):
+            with fs.open(f"{bucket}/{folder}/mseed_list/{year}_{network}_3c.txt", "r") as fp:
+                mseed_list.extend(fp.read().splitlines())
+
     mseed_list = sorted(list(set(mseed_list)))
+    print(f"Total number of mseed files: {len(mseed_list)}")
+
+    folder = "NC/phasenet"
+    # processed = set(fs.glob(f"{bucket}/{folder}/**/*.csv"))
+    processed = []
+    for network in tqdm(networks, desc="Count processed"):
+        processed.extend(fs.glob(f"{bucket}/{folder}/{network}/{year}/????.???/*.csv"))
+    processed = set(processed)
+    mseed_csv_set = set()
+    mapping_dit = {}
+    for mseed in tqdm(mseed_list, desc="Filter processed"):
+        tmp = mseed.split(",")[0].lstrip("s3://").split("/")
+        parant_dir = "/".join(tmp[2:-1])
+        fname = tmp[-1].rstrip(".mseed") + ".csv"
+        tmp_name = f"{bucket}/{folder}/{parant_dir}/{fname}"
+        mseed_csv_set.add(tmp_name)
+        mapping_dit[tmp_name] = mseed
+    mseed_csv_set = list(mseed_csv_set - processed)
+    unprocess = sorted([mapping_dit[x] for x in mseed_csv_set], reverse=True, key=lambda x: "/".join(x.split("/")[-2:]))
+    print(f"Unprocessed sample {len(unprocess)}")
+
+    with fs.open(f"{bucket}/{folder}/mseed_list/{year}_3c.txt", "w") as fp:
+        fp.write("\n".join(unprocess))
+
+    return f"{bucket}/{folder}/mseed_list/{year}_3c.txt"
+
+
+@dsl.component(base_image="zhuwq0/phasenet-ncedc:v1.2", packages_to_install=["fsspec", "gcsfs", "s3fs"])
+def run_phasenet(
+    root_path: str,
+    region: str,
+    config: Dict,
+    rank: int = 0,
+    model_path: str = "./",
+    mseed_list: str = "mseed_list.txt",
+    protocol: str = "s3",
+    bucket: str = "",
+    token: Dict = None,
+) -> int:
+    # %%
+    import json
+    import os
+    from collections import defaultdict
+    from glob import glob
+
+    import fsspec
+    import obspy
+    from tqdm import tqdm
+
+    # %%
+    fs = fsspec.filesystem("gs", token=token)
+
+    with fs.open(mseed_list, "r") as fp:
+        mseed_list = fp.read().splitlines()
 
     with open("mseed_list.txt", "w") as fp:
-        fp.write("\n".join(mseed_list[rank::world_size]))
-
-    print(
-        f"Total number of mseed files: {len(mseed_list[rank::world_size])}/{len(mseed_list)}, rank: {rank}, world_size: {world_size}"
-    )
+        fp.write("\n".join(mseed_list[rank :: config["world_size"]]))
 
     # %%
     os.system(
@@ -112,8 +166,23 @@ if __name__ == "__main__":
     with open(token_json, "r") as fp:
         token = json.load(fp)
 
+    # year = 2012
+    # year = 2013
+    # year = 2014
+    year = 2015
+    # year = 2023
+    print(f"{year = }")
+
+    # mseed_list = get_mseed_list.execute(root_path="./", region="NC", config={}, year=year, token=token)
+    # print(f"{mseed_list = }")
     # run_phasenet.execute(
-    #     root_path="./", region="NC", config={"world_size": 1}, rank=0, token=token, model_path="../PhaseNet/"
+    #     root_path="./",
+    #     region="NC",
+    #     config={"world_size": 1},
+    #     rank=0,
+    #     token=token,
+    #     model_path="../PhaseNet/",
+    #     mseed_list=mseed_list,
     # )
     # raise
 
@@ -129,24 +198,28 @@ if __name__ == "__main__":
     if not yaml_path.exists():
         yaml_path.mkdir()
 
-    world_size = 128
+    world_size = 128 * 4
 
     @dsl.pipeline
     def run_pipeline(root_path: str, region: str, config: Dict, token: Dict = None):
+        mseed_op = get_mseed_list(root_path=root_path, region=region, config=config, year=year, token=token)
+        mseed_op.set_cpu_request("2100m")
+        mseed_op.set_memory_request("12000Mi")
         with dsl.ParallelFor(items=[i for i in range(world_size)], parallelism=world_size) as item:
             # for item in [0]:
-            op = run_phasenet(
+            phasenet_op = run_phasenet(
                 root_path=root_path,
                 region=region,
                 config=config,
+                mseed_list=mseed_op.output,
                 rank=item,
                 token=token,
             )
-            op.set_cpu_request("1100m")
-            op.set_memory_request("12000Mi")
-            op.set_retry(3)
+            phasenet_op.set_cpu_request("2100m")
+            phasenet_op.set_memory_request("12000Mi")
+            # phasenet_op.set_retry(3)
 
-    client = Client("https://60381a1245c5a95b-dot-us-west1.pipelines.googleusercontent.com")
+    client = Client("https://732859323117dcc5-dot-us-west1.pipelines.googleusercontent.com")
     run = client.create_run_from_pipeline_func(
         run_pipeline,
         arguments={"token": token, "region": "NC", "root_path": "./", "config": {"world_size": world_size}},
