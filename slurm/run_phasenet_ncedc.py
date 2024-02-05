0f113534d19e1ce26ae0300b062f066c0479402b
zhuwq <zhuwq0@gmail.com>
2024-02-05 15:12:39 -0800

    update slurm/run_phasenet_ncedc.py

--- slurm/run_phasenet_ncedc.py
+++ slurm/run_phasenet_ncedc.py
@@ -1,7 +1,13 @@
 # %%
+import argparse
+import json
+import os
+from pathlib import Path
 from typing import Dict, List
 
-from kfp import dsl
+import pandas as pd
+from kfp import compiler, dsl
+from kfp.client import Client
 
 
 @dsl.component(packages_to_install=["fsspec", "gcsfs", "s3fs", "tqdm"])
@@ -11,6 +17,7 @@ def get_mseed_list(
     config: Dict,
     rank: int = 0,
     year: int = 2023,
+    stations: List = None,
     protocol: str = "s3",
     bucket: str = "",
     token: Dict = None,
@@ -74,19 +81,31 @@ def get_mseed_list(
     #     futures = [executor.submit(process, year) for year in range(1999, 2024)]
 
     # raise
+
     # %%
     fs = fsspec.filesystem("gs", token=token)
     bucket = "quakeflow_catalog"
     folder = "NC"
     networks = fs.glob(f"{bucket}/{folder}/mseed_list/{year}_??_3c.txt")
     networks = [x.split("/")[-1].split("_")[1] for x in networks]
-    networks = ["NC", "BK"]
+    # networks = ["NC", "BK"]
     mseed_list = []
     for network in networks:
         if fs.exists(f"{bucket}/{folder}/mseed_list/{year}_{network}_3c.txt"):
             with fs.open(f"{bucket}/{folder}/mseed_list/{year}_{network}_3c.txt", "r") as fp:
                 mseed_list.extend(fp.read().splitlines())
 
+    # %%
+    if stations is not None:
+        mseed_list_filt = []
+        for mseed in mseed_list:
+            station_id = ".".join(mseed.split(",")[0].split("/")[-1].split(".")[:3])
+            if station_id[:-1] in stations:
+                mseed_list_filt.append(mseed)
+        print(f"Number of selected mseed files: {len(mseed_list_filt)} / {len(mseed_list)}")
+        mseed_list = mseed_list_filt
+
+    # %%
     mseed_list = sorted(list(set(mseed_list)))
     print(f"Total number of mseed files: {len(mseed_list)}")
 
@@ -108,6 +127,9 @@ def get_mseed_list(
     mseed_csv_set = list(mseed_csv_set - processed)
     unprocess = sorted([mapping_dit[x] for x in mseed_csv_set], reverse=True, key=lambda x: "/".join(x.split("/")[-2:]))
     print(f"Unprocessed sample {len(unprocess)}")
+
+    # %%
+    # raise
 
     with fs.open(f"{bucket}/{folder}/mseed_list/{year}_3c.txt", "w") as fp:
         fp.write("\n".join(unprocess))
@@ -146,6 +168,9 @@ def run_phasenet(
     with open("mseed_list.txt", "w") as fp:
         fp.write("\n".join(mseed_list[rank :: config["world_size"]]))
 
+    if len(mseed_list[rank :: config["world_size"]]) == 0:
+        return 0
+
     # %%
     os.system(
         f"python {model_path}/phasenet/predict.py --model={model_path}/model/190703-214543 --data_list=mseed_list.txt  --format=mseed --amplitude --batch_size=1 --sampling_rate=100 --highpass_filter=1.0 --result_dir={root_path}/picks/"
@@ -155,22 +180,28 @@ def run_phasenet(
 
 
 if __name__ == "__main__":
-    import json
-    import os
-    from pathlib import Path
-
-    from kfp import compiler
-    from kfp.client import Client
+    # %%
+    parser = argparse.ArgumentParser(description="Run PhaseNet on NCEDC data")
+    parser.add_argument("--year", type=int, default=2023, help="Year to process")
+    parser.add_argument("--world_size", type=int, default=64, help="Number of workers")
+    args = parser.parse_args()
 
     token_json = f"{os.environ['HOME']}/.config/gcloud/application_default_credentials.json"
     with open(token_json, "r") as fp:
         token = json.load(fp)
 
+    # %%
+    stations = pd.read_csv("stations_ncedc.csv")
+    stations["station_id"] = stations["station"] + "." + stations["network"] + "." + stations["instrument"]
+    stations = stations["station_id"].unique().tolist()
+
     # year = 2012
     # year = 2013
     # year = 2014
-    year = 2015
+    # year = 2015
+    # year = 2022
     # year = 2023
+    year = args.year
     print(f"{year = }")
 
     # mseed_list = get_mseed_list.execute(root_path="./", region="NC", config={}, year=year, token=token)
@@ -198,11 +229,13 @@ if __name__ == "__main__":
     if not yaml_path.exists():
         yaml_path.mkdir()
 
-    world_size = 128 * 4
+    world_size = args.world_size
 
     @dsl.pipeline
     def run_pipeline(root_path: str, region: str, config: Dict, token: Dict = None):
-        mseed_op = get_mseed_list(root_path=root_path, region=region, config=config, year=year, token=token)
+        mseed_op = get_mseed_list(
+            root_path=root_path, region=region, config=config, year=year, stations=stations, token=token
+        )
         mseed_op.set_cpu_request("2100m")
         mseed_op.set_memory_request("12000Mi")
         with dsl.ParallelFor(items=[i for i in range(world_size)], parallelism=world_size) as item:
@@ -219,7 +252,7 @@ if __name__ == "__main__":
             phasenet_op.set_memory_request("12000Mi")
             # phasenet_op.set_retry(3)
 
-    client = Client("https://732859323117dcc5-dot-us-west1.pipelines.googleusercontent.com")
+    client = Client("https://73c46fe6295f8196-dot-us-west1.pipelines.googleusercontent.com")
     run = client.create_run_from_pipeline_func(
         run_pipeline,
         arguments={"token": token, "region": "NC", "root_path": "./", "config": {"world_size": world_size}},
