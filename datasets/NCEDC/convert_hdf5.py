52e16d24ee03b204a173094100c6f9974ea37ffa
JunhaoSong <42725077+JunhaoSong@users.noreply.github.com>
2024-02-06 21:52:22 -0800

    Update convert_hdf5.py (output ENZ waveforms)

--- datasets/NCEDC/convert_hdf5.py
+++ datasets/NCEDC/convert_hdf5.py
@@ -15,6 +15,10 @@ import obspy
 import pandas as pd
 from tqdm import tqdm
 
+from obspy.signal.rotate import rotate2zne
+from obspy import Inventory
+from obspy import read_inventory
+
 # warnings.filterwarnings("error")
 os.environ["OPENBLAS_NUM_THREADS"] = "2"
 
@@ -23,54 +27,28 @@ protocol = "gs"
 token = f"{os.environ['HOME']}/.config/gcloud/application_default_credentials.json"
 # token = "cloud"
 bucket = "quakeflow_dataset"
+fs = fsspec.filesystem(protocol=protocol, token=token)
 
 # root_path = "dataset"
 region = "NC"
 root_path = f"{bucket}/{region}"
 mseed_path = f"{root_path}/waveform_mseed2"
 catalog_path = f"{root_path}/catalog"
-station_path = f"{root_path}/station"
+#station_path = f"{root_path}/station"
+station_path = f"{root_path}/FDSNstationXML"
+xml_files = fs.glob(f"{station_path}/*.info/*.FDSN.xml/*.*.xml")
+inventory = Inventory()
+print("reading stationxml files to create an channel-level inventory")
+print("it takes around 15 minutes")
+for xml_file in tqdm(xml_files):
+    with fs.open(xml_file, 'rb') as f:
+        inventory.extend(read_inventory(f, level='channel'))
 result_path = f"waveform_h5"
 if not os.path.exists(result_path):
     os.makedirs(result_path)
 
-# %%
-fs = fsspec.filesystem(protocol=protocol, token=token)
-
-# %%
-comp = ["3", "2", "1", "U", "V", "E", "N", "Z"]
-order = {key: i for i, key in enumerate(comp)}
-comp2idx = {
-    "3": 0,
-    "2": 1,
-    "1": 2,
-    "U": 0,
-    "V": 1,
-    "E": 0,
-    "N": 1,
-    "Z": 2,
-}  ## only for cases less than 3 components
-
 sampling_rate = 100
 NT = 12000  # 120 s
-
-# %%
-stations = []
-for csv in fs.glob(f"{station_path}/*.csv"):
-    with fs.open(csv, "rb") as f:
-        stations.append(
-            pd.read_csv(
-                f,
-                parse_dates=["begin_time", "end_time"],
-                date_format="%Y-%m-%dT%H:%M:%S",
-                dtype={"network": str, "station": str, "location": str, "instrument": str},
-            )
-        )
-stations = pd.concat(stations)
-stations["location"] = stations["location"].fillna("")
-stations.set_index(["network", "station", "location", "instrument"], inplace=True)
-stations = stations.sort_index()
-
 
 # %%
 def calc_snr(data, index0, noise_window=300, signal_window=300, gap_window=50):
@@ -101,6 +79,7 @@ def extract_pick(picks, begin_time, sampling_rate):
     phase_time = []
     phase_polarity = []
     phase_remark = []
+    phase_picking_channel = []
     event_id = []
     for idx, pick in picks.sort_values("phase_time").iterrows():
         phase_type.append(pick.phase_type)
@@ -109,20 +88,66 @@ def extract_pick(picks, begin_time, sampling_rate):
         phase_time.append(pick.phase_time.strftime("%Y-%m-%dT%H:%M:%S.%f"))
         phase_remark.append(pick.phase_remark)
         phase_polarity.append(pick.phase_polarity)
+        phase_picking_channel.append(pick.instrument+pick.component)
         event_id.append(pick.event_id)
 
-    return phase_type, phase_index, phase_score, phase_time, phase_remark, phase_polarity, event_id
+    return phase_type, phase_index, phase_score, phase_time, phase_remark, phase_polarity, phase_picking_channel, event_id
 
-
+# %%
+def flip_polarity(phase_polarity, pha_channel_dips):
+    pol_out = []
+    for pol, cha_dip in zip(phase_polarity, pha_channel_dips):
+        if pol == 'U' or pol == '+':
+            if cha_dip == -90:
+                pol_out.append('U')
+            elif cha_dip == 90:
+                pol_out.append('D')
+            else:
+                pol_out.append('N')
+        elif pol == 'D' or pol == '-':
+            if cha_dip == -90:
+                pol_out.append('D')
+            elif cha_dip == 90:
+                pol_out.append('U')
+            else:
+                pol_out.append('N')
+        else:
+            pol_out.append('N')
+    return pol_out
+    
+# Refer to: https://github.com/ltauxe/Python-for-Earth-Science-Students/blob/master/Lecture_22.ipynb
+def dir2cart(Dir):
+    """
+    converts polar directions to cartesian coordinates
+    Parameters: 
+        Dir[Azimuth,Plunge]:  directions in degreess
+    Returns:
+        [X,Y,Z]: cartesian coordinates
+    """
+    Az,Pl=np.radians(Dir[0]),np.radians(Dir[1])
+    return np.array([np.cos(Az)*np.cos(Pl),np.sin(Az)*np.cos(Pl),np.sin(Pl)])
+def cart2dir(X):
+    """
+    converts cartesian coordinates to polar azimuth and plunge
+    Parameters:
+        X: list of X,Y,Z coordinates
+    Returns: 
+        [Az,Pl]: list of polar coordinates in degrees
+    """
+    R=np.sqrt(X[0]**2+X[1]**2+X[2]**2) # calculate resultant vector length
+    Az=np.degrees(np.arctan2(X[1],X[0]))%360. # calculate declination taking care of correct quadrants (arctan2) and making modulo 360.
+    Pl=np.degrees(np.arcsin(X[2]/R)) # calculate inclination (converting to degrees) #
+    return [Az,Pl]
+    
 # %%
 def convert(i, year):
     # %%
     fs_ = fsspec.filesystem(protocol=protocol, token=token)
-
+    
     # %%
-    with h5py.File(f"{result_path}/{year}.h5", "w") as fp:
+    with h5py.File(f"{result_path}/{year}.h5", "w") as fp, open(f"{result_path}/{year}.skipped.csv", "w") as cw:
         jdays = sorted(fs_.ls(f"{mseed_path}/{year}"), reverse=False)
-        jdays = [x.split("/")[-1] for x in jdays]
+        jdays = [x.split("/")[-1] for x in jdays][0:1]
         for jday in tqdm(jdays, total=len(jdays), desc=f"{year}", position=i, leave=True):
             tmp = datetime.strptime(jday, "%Y.%j")
 
@@ -151,14 +176,10 @@ def convert(i, year):
             phases = phases.sort_index()
 
             event_ids = sorted(fs_.ls(f"{mseed_path}/{year}/{jday}"), reverse=True)
-            event_fnames = [x.split("/")[-1] for x in event_ids]
-            event_ids = [x.split("/")[-1].split("_")[0] for x in event_ids]
-            for event_id, event_fname in zip(event_ids, event_fnames):
+            event_ids = [x.split("/")[-1] for x in event_ids][0:10]
+            for tmp_event_id in event_ids:
+                event_id, tmp_time_string = tmp_event_id.split('_')
                 if event_id not in events.index:
-                    continue
-
-                if event_id in fp:
-                    print(f"Duplicate {event_id}: {event_fname}")
                     continue
 
                 gp = fp.create_group(event_id)
@@ -169,9 +190,9 @@ def convert(i, year):
                 gp.attrs["depth_km"] = events.loc[event_id, "depth_km"]
                 gp.attrs["magnitude"] = events.loc[event_id, "magnitude"]
                 gp.attrs["magnitude_type"] = events.loc[event_id, "magnitude_type"]
-                gp.attrs["source"] = region
+                gp.attrs["source"] = "NC"
 
-                mseed_list = sorted(list(fs_.glob(f"{mseed_path}/{year}/{jday}/{event_fname}/*.mseed")))
+                mseed_list = sorted(list(fs_.glob(f"{mseed_path}/{year}/{jday}/{event_id}_{tmp_time_string}/*.mseed")))
                 st = obspy.Stream()
                 for file in mseed_list:
                     with fs_.open(file, "rb") as f:
@@ -192,15 +213,16 @@ def convert(i, year):
                 has_station = False
                 station_channel_ids = [x.split("/")[-1].replace(".mseed", "") for x in mseed_list]
                 for station_channel_id in station_channel_ids:
-                    ds = gp.create_dataset(station_channel_id, (3, gp.attrs["nt"]), dtype=np.float32)
+                    # select traces
+                    network, station, location, instrument = station_channel_id.split(".")
                     tr = st.select(id=station_channel_id + "?")
+                    # create dataset
+                    ds = gp.create_dataset(station_channel_id, (3, gp.attrs["nt"]), dtype=np.float32)
+                    # resampe traces
                     for t in tr:
                         if t.stats.sampling_rate != sampling_rate:
                             t.resample(sampling_rate)
-
-                    chn = [tr.stats.channel for tr in tr]
-                    chn = sorted(chn, key=lambda x: order[x[-1]])
-                    components = []
+                    # extract data from traces
                     for i, t in enumerate(tr):
                         index0 = int(
                             round(
@@ -210,10 +232,8 @@ def convert(i, year):
                         )
                         if index0 > 3000:
                             del fp[f"{event_id}/{station_channel_id}"]
+                            cw.writelines(f"{event_id},{station_channel_id},data_coverage_reason\n")
                             break
-
-                        if len(chn) != 3:
-                            i = comp2idx[t.stats.channel[-1]]
 
                         if index0 > 0:
                             it1 = 0
@@ -229,56 +249,104 @@ def convert(i, year):
                             ll = min(len(t.data), len(ds[i, :]))
 
                         ds[i, it2 : it2 + ll] = (t.data - np.mean(t.data))[it1 : it1 + ll] * 1e6
-                        components.append(t.stats.channel[-1])
 
                     if index0 > 3000:
                         continue
-                    network, station, location, instrument = station_channel_id.split(".")
+                    # get channel orientations
+                    orientations = []
+                    for t in tr:
+                        try:
+                            orientations.append(inventory.get_channel_metadata(t.id, arrival_time))
+                        except:
+                            pass
+                    # if channel metadata is missing or no data
+                    if len(orientations) != len(tr) or len(tr) == 0:
+                        del fp[f"{event_id}/{station_channel_id}"]
+                        cw.writelines(f"{event_id},{station_channel_id},metadata_not_found_reason\n")
+                        continue
+                    # rotate to ENZ directions
+                    # if 3 components available
+                    if len(orientations) == 3:
+                        azimuth1, dip1 = orientations[0]['azimuth'], orientations[0]['dip']
+                        azimuth2, dip2 = orientations[1]['azimuth'], orientations[1]['dip']
+                        azimuth3, dip3 = orientations[2]['azimuth'], orientations[2]['dip']
+                        vec1, vec2, vec3 = dir2cart([azimuth1, dip1]), dir2cart([azimuth2, dip2]), dir2cart([azimuth3, dip3])
+                    # if 2 components available
+                    if len(orientations) == 2:
+                        azimuth1, dip1 = orientations[0]['azimuth'], orientations[0]['dip']
+                        azimuth2, dip2 = orientations[1]['azimuth'], orientations[1]['dip']
+                        vec1 = dir2cart([azimuth1, dip1])
+                        vec2 = dir2cart([azimuth2, dip2])
+                        vec3 = np.cross(vec1, vec2)
+                        azimuth3, dip3 = cart2dir(vec3)
+                    # if 1 component available
+                    if len(orientations) == 1:
+                        azimuth1, dip1 = orientations[0]['azimuth'], orientations[0]['dip']
+                        vec1 = dir2cart([azimuth1, dip1])
+                        vec2 = np.random.randn(3)
+                        vec2 -= vec2.dot(vec1) * vec1
+                        vec2 /= np.linalg.norm(vec2)
+                        azimuth2, dip2 = cart2dir(vec2)
+                        vec3 = np.cross(vec1, vec2)
+                        azimuth3, dip3 = cart2dir(vec3)
+                    # E->[90, 0]
+                    # N->[0, 0]
+                    # Z->[0, -90]
+                    nearzero = 1e-4
+                    if abs(np.dot(vec1, vec2)) > nearzero or abs(np.dot(vec1, vec3)) > nearzero or abs(np.dot(vec2, vec3)) > nearzero:
+                        del fp[f"{event_id}/{station_channel_id}"]
+                        cw.writelines(f"{event_id},{station_channel_id},channel_not_orthogonal_reason\n")
+                        continue
+                    znewaveforms = rotate2zne(ds[0, :], azimuth1, dip1,
+                                            ds[1, :], azimuth2, dip2,
+                                            ds[2, :], azimuth3, dip3)
+                    ds[:] = np.array(znewaveforms)[::-1]
+                    ds.attrs["component"] = 'ENZ'
+                    # assign zeros if one component has very low values
+                    asmallvalue = np.max(np.abs(ds[:])) / 1e10
+                    for i in range(3):
+                        if np.max(np.abs(ds[i, :])) < asmallvalue:
+                            ds[i, :] = 0
+                    # other attibutes
                     ds.attrs["network"] = network
                     ds.attrs["station"] = station
                     ds.attrs["location"] = location
                     ds.attrs["instrument"] = instrument
-                    ds.attrs["component"] = "".join(components)
                     ds.attrs["unit"] = "1e-6m/s" if instrument[-1] != "N" else "1e-6m/s**2"
                     ds.attrs["dt_s"] = 0.01
-
-                    sta = (
-                        stations.loc[(network, station, location, instrument)]
-                        .sort_values("begin_time", ascending=False)
-                        .iloc[0]
-                    )
-                    ds.attrs["longitude"] = sta["longitude"]
-                    ds.attrs["latitude"] = sta["latitude"]
-                    ds.attrs["elevation_m"] = sta["elevation_m"]
-                    ds.attrs["local_depth_m"] = sta["local_depth_m"]
-                    ds.attrs["depth_km"] = sta["depth_km"]
-
+                    # at least one channel is available
+                    ds.attrs["longitude"] = orientations[0]["longitude"]
+                    ds.attrs["latitude"] = orientations[0]["latitude"]
+                    ds.attrs["elevation_m"] = orientations[0]["elevation"]
+                    ds.attrs["local_depth_m"] = orientations[0]["local_depth"]
+                    ds.attrs["depth_km"] = round(-0.001*(ds.attrs["elevation_m"]-ds.attrs["local_depth_m"]), 4)
                     station_id = f"{network}.{station}.{location}"
                     if station_id not in phases_by_station.index:
                         del fp[f"{event_id}/{station_channel_id}"]
+                        cw.writelines(f"{event_id},{station_channel_id},no_phase_found_reason1\n")
                         continue
                     picks_ = phases_by_station.loc[[station_id]]
                     picks_ = picks_[(picks_["phase_time"] > begin_time) & (picks_["phase_time"] < end_time)]
                     if len(picks_[picks_["event_id"] == event_id]) == 0:
                         del fp[f"{event_id}/{station_channel_id}"]
+                        cw.writelines(f"{event_id},{station_channel_id},no_phase_found_reason2\n")
                         continue
 
                     pick = picks_[picks_["event_id"] == event_id].iloc[0]  # after sort_value
-                    if "azimuth" in pick:
-                        ds.attrs["azimuth"] = pick.azimuth
-                    if "takeoff_angle" in pick:
-                        ds.attrs["distance_km"] = pick.distance_km
-                    if "takeoff_angle" in pick:
-                        ds.attrs["takeoff_angle"] = pick.takeoff_angle
+                    ds.attrs["azimuth"] = pick.azimuth
+                    ds.attrs["distance_km"] = pick.distance_km
+                    ds.attrs["takeoff_angle"] = pick.takeoff_angle
 
                     tmp = int(round((pick.phase_time - begin_time).total_seconds() * sampling_rate))
                     if (tmp - 300 < 0) or (tmp + 300 >= NT):
                         del fp[f"{event_id}/{station_channel_id}"]
+                        cw.writelines(f"{event_id},{station_channel_id},snr_timewindow_reason\n")
                         continue
 
                     snr = calc_snr(ds[:, :], int(round((pick.phase_time - begin_time).total_seconds() * sampling_rate)))
                     if max(snr) == 0:
                         del fp[f"{event_id}/{station_channel_id}"]
+                        cw.writelines(f"{event_id},{station_channel_id},zero_snr_reason\n")
                         continue
                     ds.attrs["snr"] = snr
 
@@ -289,15 +357,26 @@ def convert(i, year):
                         phase_time,
                         phase_remark,
                         phase_polarity,
+                        phase_picking_channel, 
                         phase_event_id,
                     ) = extract_pick(picks_, begin_time, sampling_rate)
-
+                    # flip the P polarity if the vertical channel is reversed
+                    phase_picking_channel_x = [".".join([station_id, x]) for x in phase_picking_channel]
+                    pha_channel_dips = []
+                    for x in phase_picking_channel_x:
+                        try:
+                            pha_channel_dips.append(inventory.get_channel_metadata(x, arrival_time)['dip'])
+                        except:
+                            pha_channel_dips.append('none')
+                    phase_polarity = flip_polarity(phase_polarity, pha_channel_dips)
+                    # 
                     ds.attrs["phase_type"] = phase_type
                     ds.attrs["phase_index"] = phase_index
                     ds.attrs["phase_score"] = phase_score
                     ds.attrs["phase_time"] = phase_time
                     ds.attrs["phase_remark"] = phase_remark
                     ds.attrs["phase_polarity"] = phase_polarity
+                    ds.attrs["phase_picking_channel"] = phase_picking_channel
                     ds.attrs["event_id"] = phase_event_id
 
                     if (
@@ -315,8 +394,6 @@ def convert(i, year):
                 if not has_station:
                     print(f"{event_id} has no station")
                     del fp[event_id]
-
-            # return
 
 
 if __name__ == "__main__":
