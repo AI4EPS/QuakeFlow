30759d80dc4be4d3d175f74c41a269d5ee989930
Weiqiang Zhu <75299929+zhuwq0@users.noreply.github.com>
2024-02-20 23:50:52 -0800

    Update merge_ncedc.py

--- slurm/merge_ncedc.py
+++ slurm/merge_ncedc.py
@@ -18,16 +18,24 @@ folder = "NC/phasenet"
 fs = fsspec.filesystem(protocol, token=token)
 
 # %%
-csv_list = fs.glob(f"{bucket}/{folder}/??/2023/2023.365/*.csv")  ## choose year and jday
-print(f"Save {len(csv_list)} CSV files")
-with open("csv_list.txt", "w") as fp:
-    fp.write("\n".join(csv_list))
+# year = 2023
+# csv_list = fs.glob(f"{bucket}/{folder}/??/{year}/{year}.???/*.csv")  ## choose year and jday
+# csv_list = [f"{protocol}://{csv}" for csv in csv_list]
+# print(f"Save {len(csv_list)} CSV files")
+# with open(f"{year}.txt", "w") as fp:
+#     fp.write("\n".join(csv_list))
+
+# with open(f"{year}.txt", "r") as fp:
+#     csv_list = fp.read().splitlines()
 
 # %%
-with open("csv_list.txt", "r") as fp:
-    csv_list = fp.read().split("\n")
+year = 2023
+with fs.open(f"{bucket}/{folder}/csv_list/{year}.txt", "r") as fp:
+    csv_list = fp.read().splitlines()
 
 for csv in csv_list:
-    df = pd.read_csv(fs.open(csv, "r"))
+    df = pd.read_csv(fs.open(csv, token=token))
     print(df)
     raise
+
+# %%
